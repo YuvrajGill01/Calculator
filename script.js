1a6cf896e0aa83c9b699a00087a8384e8ccc03eb
YuvrajGill01 <yuvrajsinghg213@gmail.com>
2025-01-24 10:42:32 +0530

    code optimized

--- script.js
+++ script.js
@@ -9,21 +9,6 @@ function clear1() {
   document.getElementById("inpId").value = updValue;
 }
 
-// document.getElementById('myButton')
-// .addEventListener('click', function () {
-//     // alert('Button clicked!');
-//     let addvalue = document.getElementById('myButton').innerText;
-//     document.getElementById('inpId').value += addvalue;
-//     console.log(document.getElementById('myButton').innerText)
-// });
-
-// document.getElementsByClassName('addStrInp')
-// .addEventListener('click', function () {
-//     let addvalue = document.getElementsByClassName('addStrInp').innerText;
-//     document.getElementById('inpId').value += addvalue;
-//     console.log(document.getElementsByClassName('addStrInp').innerText)
-// });
-
 // ADDING THE NUMBERS IN INPUT FIELD
 const arr = document.querySelectorAll(".addStrInp");
 console.log(arr);
@@ -40,29 +25,17 @@ for (let btn of newArr) {
 
     const prev = document.getElementById("inpId").value;
     console.log("prevValue", prev);
-    //['+', '-', '*', '/','%'].includes(prev.charAt(prev.length-1))
 
     if (replaces == true) {
       document.getElementById("inpId").value = this.innerText;
-      // dummyStr++;
       replaces = false;
     }else{
       document.getElementById("inpId").value += this.innerText;
     }
-
-
-    
   });
 }
 
-// ADDING THE OPERATORS IN INPUT FIELD
-const arr2 = document.querySelectorAll(".addStrInp2");
-console.log(arr2);
-
-const newArr2 = Array.from(arr2);
-console.log(newArr2);
-let dotAllowed = true;
-
+//CHECK FUNCTION FOR DECIMAL SIGNIFICANCY
 function dotContainsSignificantly(expression){
   let arrExpDot = expression.split("");
   let dotCount = 0;
@@ -87,6 +60,14 @@ function dotContainsSignificantly(expression){
   return dotAllowed;
 }
 
+// ADDING THE OPERATORS IN INPUT FIELD
+const arr2 = document.querySelectorAll(".addStrInp2");
+console.log(arr2);
+
+const newArr2 = Array.from(arr2);
+console.log(newArr2);
+
+let dotAllowed = true;
 
 for (let btn2 of newArr2) {
   btn2.addEventListener("click", function () {
@@ -110,12 +91,6 @@ for (let btn2 of newArr2) {
       (prev.charAt(prev.length - 1) == "%" &&
         ["+", "-", "*", "/", "%"].includes(this.innerText))
     ) {
-      // console.log(
-      //   "Operator replaces previous operator if",
-      //   prev.charAt(prev.length - 1), " " ,this.innerText,
-      //   " ",
-      //   typeof this.innerText
-      // );
       document.getElementById("inpId").value =
         prev.slice(0, prev.length - 1) + this.innerText;
     } 
@@ -141,66 +116,7 @@ for (let btn2 of newArr2) {
   });
 }
 
-// document.getElementsByClassName("addStrInp2").addEventListener("click", function () {
-//   let addvalue = document.querySelectorAll(".addStrInp2").innerText;
-//   console.log(addvalue);
-//   let prev = document.getElementById("inpId").value;
 
-//   if (/\d/.test(prev.charAt(prev.length - 1))) {
-//     document.getElementById("inpId").value += addvalue;
-//   } else {
-//     return;
-//   }
-// });
-
-//METHOD 1:- USING IN-BUILT EVAL FUNCTION
-function evaluateFun() {
-  var inpValue = document.getElementById("inpId").value;
-  var ans = eval(inpValue);
-  if (isNaN(ans)) {
-    return;
-  } else {
-    document.getElementById("inpId").value = ans;
-  }
-  console.log(ans);
-}
-
-//METHOD 2:- CUSTOM EVAL FUNCTION FOR CALCULATING THE EXPRESSION
-function evaluateFun2() {
-  var inpValue = document.getElementById("inpId").value;
-
-  if (inpValue === "") {
-    alert("Haha ,Enter the expression correctly!!");
-    return;
-  }
-
-  try {
-    var result = evaluateExpression(inpValue);
-
-    // Display the result
-    document.getElementById("inpId").value = result;
-  } catch (e) {
-    alert("Invalid expression. Please enter a valid mathematical expression.");
-  }
-}
-function evaluateExpression(inpValue) {
-  // Remove spaces from the expression
-  inpValue = inpValue.replace(" ", "");
-
-  // Regular expression to match valid characters (numbers and operators)
-  var regex = /^[0-9+\-*%/().]*$/;
-
-  // Check if the expression contains invalid characters
-  if (!regex.test(inpValue)) {
-    throw new Error("Invalid characters detected.");
-  }
-
-  // Use JavaScript's built-in `Function` constructor to safely evaluate the expression
-  // This is much safer than `eval()`, as it only allows a specific pattern.
-  var result = new Function("return " + inpValue)();
-
-  return result;
-}
 
 //METHOD 3:- By converting first the string into an array and then evaluating the expression
 function evaluateFun3() {
@@ -211,7 +127,7 @@ function evaluateFun3() {
     return;
   }
   var arr = convertToArray(inpValue);
-  // console.log(arr);
+   console.log(arr);
   let finalResult = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] == "+" && i < arr.length) {
